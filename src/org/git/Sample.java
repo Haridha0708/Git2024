1c21802bd28fc2a0a06f482843f250f9178d9ca1
Haridha0708 <haridhasethu@gmail.com>
2024-08-16 23:21:06 +0530

    My first task

---
+++ src/org/git/Sample.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class Sample {
+	public void id() {
+		System.out.println("I'D :8476");
+	}
+	public static void main(String[] args) {
+		Sample a = new Sample();
+		a.id();
+	}
+
+}
