7af2689c57e50146b1920e78dbe7c9e7736cf469
Haridha0708 <haridhasethu@gmail.com>
2024-08-17 15:21:16 +0530

    Member3

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -4,19 +4,16 @@ public class Sample {
 	public void id() {
 		System.out.println("I'D :8476");
 	}
-<<<<<<< HEAD
-	public void clgName() {
-		System.out.println("College Name:Pavai Arts and Science for women");
-	}
-=======
 	public void name() {
 		System.out.println("Name:Haridha");
 	}
 	public void depart() {
 		System.out.println("Department:Mathematics");
 	}
-	
->>>>>>> 69cf19d516579a22a56a91702a01fe8b801f3ab7
+	public void clgName() {
+		System.out.println("College Name:Pavai Arts and Science for women");
+	}
+
 	public static void main(String[] args) {
 		Sample a = new Sample();
 		a.id();
