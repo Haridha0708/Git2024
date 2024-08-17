c5e8139e852d9356c962d0d8a53068c9f03f53d3
Haridha0708 <haridhasethu@gmail.com>
2024-08-17 08:23:58 +0530

    Member2 do some changes

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -4,6 +4,9 @@ public class Sample {
 	public void id() {
 		System.out.println("I'D :8476");
 	}
+	public void depart() {
+		System.out.println("Department:Mathematics");
+	}
 	public static void main(String[] args) {
 		Sample a = new Sample();
 		a.id();
