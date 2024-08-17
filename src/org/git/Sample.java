c990533dd5d31351c741d1925f6092584db76cfc
Haridha0708 <haridhasethu@gmail.com>
2024-08-17 14:50:46 +0530

    Member3 do changes

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -4,6 +4,9 @@ public class Sample {
 	public void id() {
 		System.out.println("I'D :8476");
 	}
+	public void clgName() {
+		System.out.println("College Name:Pavai Arts and Science for women");
+	}
 	public static void main(String[] args) {
 		Sample a = new Sample();
 		a.id();
