65d0a785fe7caed0287c67bf577aaf9a6cef0ebe
Haridha0708 <haridhasethu@gmail.com>
2024-08-17 08:02:10 +0530

    My First task

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -4,6 +4,10 @@ public class Sample {
 	public void id() {
 		System.out.println("I'D :8476");
 	}
+	public void name() {
+		System.out.println("Name:Haridha");
+
+	}
 	public static void main(String[] args) {
 		Sample a = new Sample();
 		a.id();
